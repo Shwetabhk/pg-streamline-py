65462511d63d4977a54b05d2c3010e185401d8f1
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-10-10 11:21:28 +0530

    feat: Increasing coverage to 100%

--- tests/test_types.py
+++ tests/test_types.py
@@ -67,3 +67,25 @@ def test_base_not_implemented_methods():
         with pytest.raises(NotImplementedError) as excinfo:
             parser.decode_delete_message()
         assert 'This method should be overridden by subclass' in str(excinfo.value)
+
+
+# Test BaseMessage for decode_tuple for 'u' and 'n' types
+def test_decode_tuple_null_and_unchanged():
+     with mock.patch('psycopg2.connect') as mock_conn:
+        mock_con = mock_conn.return_value
+        mock_cur = mock_con.cursor.return_value
+
+        base_message_instance = BaseMessage(b'123', cursor=mock_cur)
+        # Replace with actual class instantiation if needed
+        base_message_instance.schema = {'columns': [{'name': 'col1'}, {'name': 'col2'}]}
+
+        # Mock the methods
+        base_message_instance.read_int16 = mock.MagicMock(return_value=2)
+        base_message_instance.read_string = mock.MagicMock(side_effect=['n', 'u'])
+        base_message_instance.read_int32 = mock.MagicMock()  # This won't be called
+
+        # Call the method
+        result = base_message_instance.decode_tuple()
+
+        # Assertions
+        assert result == {'col1': None, 'col2': None}
